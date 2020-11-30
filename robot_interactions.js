8577babf8eba8e2e100c1e900e3329407040cb20
Sgangloff <silvere.gangloff@gmx.com>
2020-11-30 10:47:27 +0100

    update

--- robot_interactions.js
+++ robot_interactions.js
@@ -20,17 +20,17 @@ document.addEventListener("mousemove", (e)=>{
 	c=((R-r)*(1-Math.exp(-Math.pow(Math.abs(x)+Math.abs(y),0.1)))-5),
       	alpha=c*Math.cos(theta)+121,
       	beta=c*Math.sin(theta)+121,
-    	echelle=(Math.sqrt(Math.pow(x,2)+Math.pow(y,2)))/100;
+    	echelle=(Math.sqrt(Math.pow(x,2)+Math.pow(y,2))-c)/200;
   
   	pupil.style.transform = "rotate(" + (angle) + "deg) translateX(" + ((R-r)*(1-Math.exp(-Math.pow(Math.abs(x)+Math.abs(y),0.1)))-5) + "px)";
 	pupil.style.transformOrigin =121+"px" + " " + 121 +"px";
     
-    	laser.style.transform= "translateX(" + (alpha) + "px) translateY(" + (beta) + "px) rotate(" + (angle) + "deg) scaleX(" + echelle +")";
+    	laser.style.transform= " translateX(" + (alpha) + "px) translateY(" + (beta) + "px) rotate(" + (angle) + "deg) scaleX(" + echelle +")";
 
 });
 
 var frame=document.getElementById("robot_image");
 var background=document.getElementById("background");
-//frame.addEventListener("click", (e) =>{laser.style.animation="move_laser 1s 1"; 
-//setTimeout(function() {laser.style.animation="none";}, 1100);
-//setTimeout(function() {background.style.cursor="url('files_svg/tinycursor.svg'),auto";}, 750);}); 
+frame.addEventListener("click", (e) =>{laser.style.animation="move_laser 1s 1"; 
+setTimeout(function() {laser.style.animation="none";}, 1100);
+setTimeout(function() {background.style.cursor="url('files_svg/tinycursor.svg'),auto";}, 750);}); 
